f42064b99147e6c3737ea7bef0efc2b5a4c50299
mahmudovyusif <mahmudovyusif52@gmail.com>
2025-12-26 22:02:12 +0400

    classwork_2

---
+++ src/Classworks/Classwork_2.java
@@ -0,0 +1,67 @@
+package Classworks;
+
+public class Classwork_2  {
+	public static void main(String[] args) {
+		
+		// 1
+//		int a = 5;
+//		
+//		if (a >= 18) {
+//			boolean console = true; 
+//		}else {
+//			boolean console = false; 
+//		}
+//		 
+//		 
+//		int b = 18;
+//		
+//		if( b  >=18) {
+//			boolean console = true; 
+//		}else {
+//			boolean console = false; 
+//		}
+//		
+		// 2
+		
+//		int c = 1;
+//		if( c=0)  {
+//			boolean console = false;
+//		}else {
+//			boolean console =true;
+//		}
+		// 3
+//		 Scanner scanner = new Scanner(System.in);
+//	        System.out.print("rite number ( 1 to 5): ");
+//	        int grade = scanner.nextInt();
+//
+//	        //  1:  if-else if-else
+//	        if (grade == 5) {
+//	            System.out.println("good");
+//	        } else if (grade == 4) {
+//	            System.out.println("nice");
+//	        } else if (grade == 3) {
+//	            System.out.println("not goode and not bad");
+//	        } else {
+//	            System.out.println("wery bade 1-5)");
+//	        }
+		//4
+//		 Scanner scanner = new Scanner(System.in);
+//	        
+//	        System.out.print("rite tepmeratuere: ");
+//	        
+//	        
+//	        if (scanner.hasNextDouble()) {
+//	            double temp = scanner.nextDouble();
+//
+//	            if (temp > 25) {
+//	                System.out.println("hoot");
+//	            } else if (temp >= 10) {
+//	                System.out.println("not hot");
+//	            } else {
+//	                System.out.println("colde");
+//	            }
+//	        } else {
+//	            System.out.println("otation.");
+//	        }
+	}
+}
