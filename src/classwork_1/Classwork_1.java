57efbe2768ff42275ec4577adcf53819fdf6266a
mahmudovyusif <mahmudovyusif52@gmail.com>
2026-01-06 22:04:20 +0400

    classwork_1

---
+++ src/classwork_1/Classwork_1.java
@@ -0,0 +1,9 @@
+package classwork_1;
+
+public class Classwork_1 {
+	public static void main(String[] args) {
+		
+		//
+		
+	}
+}
---
+++ src/homework_1/Homework_1.java
@@ -0,0 +1,9 @@
+package homework_1;
+
+public class Homework_1 {
+	public static void main(String[] args) {
+		
+		//
+		
+	}
+}
